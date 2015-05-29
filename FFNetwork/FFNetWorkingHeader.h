0529175f25ded2ae7003c4dac7cd35189aa38259
ixiazer <shan.xu@freshfresh.com>
2015-05-29 14:01:29 +0800

    add file

---
+++ FFNetwork/FFNetWorkingHeader.h
@@ -0,0 +1,19 @@
+//
+//  FFNetWorkingHeader.h
+//  FFNetworking
+//
+//  Created by xiazer on 15/5/7.
+//  Copyright (c) 2015年 xiazer. All rights reserved.
+//
+
+#ifndef FFNetworking_FFNetWorkingHeader_h
+#define FFNetworking_FFNetWorkingHeader_h
+
+
+#define FFNetworkingGetServiceIDForFreshFresh @"GetServiceIDForFreshFresh"
+#define FFNetworkingPostServiceIDForFreshFresh @"PostServiceIDForFreshFresh"
+#define FFNetworkingRestfulGetServiceIDForFreshFresh @"RestfulGetServiceIDForFreshFresh"
+#define FFNetworkingRestfulPostServiceIDForFreshFresh @"RestfulPostServiceIDForFreshFresh"
+
+
+#endif
